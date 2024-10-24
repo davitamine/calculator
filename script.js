2aeaef2699a74b0ee6cd18ba16902267f09260b8
davitamine <daviddeheus99@gmail.com>
2024-10-24 15:43:11 +0200

    calculations

---
+++ script.js
@@ -0,0 +1,43 @@
+/*
+const num1 = 
+
+const num2 =
+*/
+const add = function (num1, num2) {
+    return num1 + num2;
+};
+  
+const subtract = function (num1, num2) {
+    return num1 - num2;
+};
+
+const multiply = (num1, num2) => {
+    return num1 * num2;
+};
+
+const divide = (num1, num2) => {
+    return num1 / num2;
+};
+
+console.log(divide(10, 20))
+/*
+const add = (array) => {
+    return array.reduce((sum, number) => {
+        return sum += number;
+     }, 0);
+}
+
+const subtract = (array) => {
+    return array.reduce((sum, number) => {
+        return sum -= number;
+     }, 0);
+}
+
+const multiply = (array) => {
+    return array.reduce((sum, number) => {
+        return sum *= number;
+     }, 0);
+}
+
+*/
+
