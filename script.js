85b6f68b9f13a2ad22e8b35b0c3de4fd6c3b8b7b
davitamine <daviddeheus99@gmail.com>
2024-10-24 17:15:31 +0200

    tot display value firstnumber

--- script.js
+++ script.js
@@ -1,25 +1,63 @@
-/*
-const num1 = 
-
-const num2 =
-*/
-const add = function (num1, num2) {
-    return num1 + num2;
+const add = function (firstNumber, secondNumber) {
+    return firstNumber + secondNumber;
 };
   
-const subtract = function (num1, num2) {
-    return num1 - num2;
+const subtract = function (firstNumber, secondNumber) {
+    return firstNumber - secondNumber;
 };
 
-const multiply = (num1, num2) => {
-    return num1 * num2;
+const multiply = (firstNumber, secondNumber) => {
+    return firstNumber * secondNumber;
 };
 
-const divide = (num1, num2) => {
-    return num1 / num2;
+const divide = (firstNumber, secondNumber) => {
+    return firstNumber / secondNumber;
 };
 
-console.log(divide(10, 20))
+let firstNumber = null
+
+let operator = null
+
+let secondNumber = null
+
+const display = document.getElementById("display");
+
+const updateDisplay = (value) => {
+    display.value = value;
+}
+
+const getButtonValue = (value) => {
+    return value = button.textContent;
+}
+
+const numberButtons = document.querySelectorAll(".num");
+numberButtons.forEach(button => button.addEventListener("click", () => {
+    display.value += button.textContent; 
+    console.log(button.textContent);
+    console.log(display.textContent);
+    // console.log("display");
+    // console.log(display);
+    // console.log("button");
+    // console.log(button);
+}))
+
+
+
+let operate = (firstNumber, operator, secondNumber) => {
+    if (operator === "+") {
+        return add(firstNumber, secondNumber);
+    } else if (operator === "-") {
+        return subtract(firstNumber, secondNumber);
+    } else if (operator === "*") {
+        return multiply(firstNumber, secondNumber);
+    } else if (operator === "+") {
+        return divide(firstNumber, secondNumber);
+    }
+}
+
+
+
+
 /*
 const add = (array) => {
     return array.reduce((sum, number) => {
